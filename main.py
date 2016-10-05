8dd345c98de3dae388378c3cfa0e9fb84abed59c
slaxit32 <dilushait@gmail.com>
2016-10-05 23:22:36 +0530

    greedy added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from terminaltables import SingleTable
 import time
 import random
 import os
+import math
 
 #clear the screen
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -48,7 +49,9 @@ def table(title,stones,start,end,current,serch,path,final):
 
 	table_data[int(start/10)][int(start%10)]=Color("{autored}St{/autored}")
 	table_data[int(end/10)][int(end%10)]=Color("{autored}En{/autored}")
-	table_data[int(current/10)][int(current%10)]=Color('{autoyellow}O{/autoyellow}')
+
+	if(current!=""):
+		table_data[int(current/10)][int(current%10)]=Color('{autoyellow}O{/autoyellow}')
 
 
 	table_instance = SingleTable(table_data,title)
@@ -57,6 +60,8 @@ def table(title,stones,start,end,current,serch,path,final):
 	table_instance.justify_columns = {0: 'center', 1: 'center', 2: 'center'}
 
 	Windows.enable(auto_colors=True, reset_atexit=True)  # Does nothing if not on Windows.
+
+	print()
 
 	return table_instance.table
 
@@ -97,6 +102,77 @@ def nei(node):
 
 	return addStrck
 
+def neiShort(node,e):
+
+	addStrck=[]
+	dis=[]
+	disSort=[]
+	outList=[]
+
+
+	sl=0
+	el=99
+
+	u=up(node)
+	if (u>=0 and u<=99 ):
+		ud=sdis(u, e)
+		dis.append(ud)
+		addStrck.append(u)
+
+	r=ri(node)
+	if (r>=0 and r<=99 and int(r/10)==int(node/10)):
+		rd=sdis(r, e)
+		dis.append(rd)
+		addStrck.append(r)
+
+	d=do(node)
+	if (d>=0 and d<=99):
+		dd=sdis(d, e)
+		dis.append(dd)
+		addStrck.append(d)
+
+	l=le(node)
+	if (l>=0 and l<=99 and int(l/10)==int(node/10)):
+		ld=sdis(l, e)
+		dis.append(ld)
+		addStrck.append(l)
+
+
+
+	# print(addStrck)
+	# print("dis ",dis)
+	disSort=dis
+	disSort=sorted(dis)
+	# print("dissort ",disSort)
+
+	map=[]
+
+	for i in addStrck:
+		map.append([])
+
+	cou=0
+	for i in dis:
+		map[cou].append(i)
+		cou+=1
+
+	cou=0
+	for i in addStrck:
+		map[cou].append(i)
+		cou+=1
+
+
+
+
+	map.sort()
+	# print("map ",map)	
+
+
+	for i in map:
+		outList.append(i[1])
+
+	# print(outList)
+
+	return outList
 
 def checkleaf(node,vis):
 	visited=vis
@@ -120,6 +196,9 @@ def dfs(s,e):
 	en=e
 
 	cn=sn
+
+	print(table("DFS",stones,sn,en,"",stack,travel,""))
+
 	visited.append(sn)
 
 	t=[]
@@ -130,17 +209,19 @@ def dfs(s,e):
 	stack=t+stack
 	visited=t+visited
 
-	print("current node ",cn)
-	print("stack ",stack)
-	print("visited ",visited)
-	print()
+	print(table("DFS",stones,sn,en,cn,stack,travel,""))
 
-	print(table("dfs",stones,sn,en,cn,stack,"",""))
+	# print("current node ",cn)
+	# print("stack ",stack)
+	# print("visited ",visited)
+	# print()
+
+	# print(table("dfs",stones,sn,en,cn,stack,"",""))
 
 	flag=0
 
 	while cn!=en:
-		print("stack before ",stack)
+		#print("stack before ",stack)
 		
 		if(len(stack)==0):
 			print("\n\n------------------------------No path------------------------------\n\n")
@@ -159,20 +240,21 @@ def dfs(s,e):
 
 			stack=t+stack
 
-			print(table("dfs",stones,sn,en,cn,stack,travel,""))
+			print(table("DFS",stones,sn,en,cn,stack,travel,""))
+
 			#time.sleep(0.25)
 
-			print("current node ",cn)
-			print("stack after ",stack)
-			print("visited ",visited)
-			print()
+			# print("current node ",cn)
+			# print("stack after ",stack)
+			# print("visited ",visited)
+			# print()
 			i=cn
 		
-			print(stack)
+			# print(stack)
 			
 			
 	if(flag!=1):
-		print(table("dfs",stones,sn,en,cn,"","",travel))
+		print(table("DFS",stones,sn,en,"","","",travel))
 
 	
 
@@ -180,12 +262,9 @@ def dfs(s,e):
 	timeTaken=end - start
 
 	if(timeTaken<1):
-		print("Time taken ",timeTaken*1000," ms")
+		print("\nTime taken ",timeTaken*1000," ms")
 	else:
-		print("Time taken ",timeTaken," s")
-
-
-	
+		print("\nTime taken ",timeTaken," s")
 
 def bfs(s,e):
 
@@ -200,7 +279,13 @@ def bfs(s,e):
 	en=e
 
 	cn=sn
+
+	print(table("BFS",stones,sn,en,"",stack,travel,""))
+
 	visited.append(sn)
+
+	
+
 
 	t=[]
 	for i in nei(cn):
@@ -210,12 +295,15 @@ def bfs(s,e):
 	stack=t+stack
 	visited=t+visited
 
+	print(table("BFS",stones,sn,en,cn,stack,travel,""))
+
+
 	# print("current node ",cn)
 	# print("stack ",stack)
 	# print("visited ",visited)
 	# print()
 
-	print(table("bfs","",sn,en,cn,stack,"",""))
+	#print(table("bfs","",cn,en,cn,stack,"",""))
 
 	flag=0
 
@@ -235,7 +323,8 @@ def bfs(s,e):
 						visited.append(i)
 			stack=stack+t
 			travel.append(cn)
-			print(table("bfs","",sn,en,cn,stack,travel,""))
+			print(table("BFS",stones,sn,en,cn,stack,travel,""))
+
 			#time.sleep(0.1)
 			#os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -249,32 +338,116 @@ def bfs(s,e):
 			
 			#print(stack)
 	if(flag!=1):
-		print(table("bfs",stones,sn,en,cn,"","",travel))
+		print(table("BFS",stones,sn,en,"","","",travel))
+		
+	end = time.time()
+	timeTaken=end - start
+
+	if(timeTaken<1):
+		print("\nTime taken ",timeTaken*1000," ms")
+	else:
+		print("\nTime taken ",timeTaken," s")
+
+def sdis(a,b):
+	x1=int(a/10)
+	y1=int(a%10)
+
+	x2=int(b/10)
+	y2=int(b%10)
+
+	t=math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+	return t
+
+def greedy(s,e):
+
+
+	start = time.time()
+
+	stack=[]
+	visited=[]
+	travel=[]
+
+	sn=s
+	en=e
+
+	cn=sn
+	visited.append(sn)
+
+	print(table("Greedy",stones,sn,en,"",stack,travel,""))
+
+
+	t=[]
+	for i in neiShort(cn,en):
+		if(i not in stones):
+			t.append(i)
+
+	stack=t+stack
+	visited=t+visited
+
+	# print("current node ",cn)
+	# print("stack ",stack)
+	# print("visited ",visited)
+	# print()
+
+	#print(table("Greedy","",sn,en,cn,stack,"",""))
+	print(table("Greedy",stones,sn,en,"",stack,travel,""))
+
+	flag=0
+
+	while cn!=en:
+		#print("stack before ",stack)
+		if(len(stack)==0):
+			print("\n\n------------------------------No path------------------------------\n\n")
+			flag=1
+			break
+		else:
+			cn=stack.pop(0)
+			t=[]
+			for i in neiShort(cn,en):
+				if(i not in visited):
+					if(i not in stones):
+						t.append(i)
+						visited.append(i)
+			stack=t+stack
+			travel.append(cn)
+			print(table("Greedy",stones,sn,en,cn,stack,travel,""))
+			#time.sleep(0.1)
+			#os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+			# print("current node ",cn)
+			# print("stack after ",stack)
+			# print("visited ",visited)
+			# print()
+			i=cn
+			
+			#print(stack)
+	if(flag!=1):
+		print(table("Greedy",stones,sn,en,"","","",travel))
 
 	end = time.time()
 	timeTaken=end - start
 
 	if(timeTaken<1):
-		print("Time taken ",timeTaken*1000," ms")
+		print("\nTime taken ",timeTaken*1000," ms")
 	else:
-		print("Time taken ",timeTaken," s")
+		print("\nTime taken ",timeTaken," s")
 
-
-startNode=random.randint(0,99)
-print("Start at ",startNode)
-endNode=random.randint(0,99)
-print("End at ",endNode)
+# startNode=random.randint(0,99)
+# print("Start at ",startNode)
+# endNode=random.randint(0,99)
+# print("End at ",endNode)
 
 
 #ading stones
-stones=[2,11]
-
-print(table("Init",stones,0,9,98,"","",""))
-
-#dfs(0,9)
-#bfs(0,9)
+stones=[24,34,44,54,64,74,4,14]
 
 
+#dfs(0,55)
+#bfs(0,55)
+#greedy(0,55)
 
 
 
@@ -293,71 +466,6 @@ print(table("Init",stones,0,9,98,"","",""))
 
 
 
-# for i in ["Hi", "You", "Fool"]:
-#     sys.stdout.write("\r" + i)
-#     sys.stdout.flush()
-#     time.sleep(1)
-
-
-# cn=sn
-# # print("start node ",sn)
-# # stack.append(up(sn))
-# # stack.append(ri(sn))
-# # stack.append(do(sn))
-# # stack.append(le(sn))
-
-# # visited.append(sn)
-
-# # print("stack before",stack)
-
-# # cn=stack.pop(0)
-# # print("current node ", cn)
-
-# # print("stack after",stack)
-# # print("visited ",visited)
-
-
-
-# while cn>=0 and cn<=99:
-
-# 	print()
-
-# 	print("current node ",cn)
-
-
-# 	l=le(cn)
-# 	if l in table_data:
-# 		print("added left")
-# 		stack.insert(0,l)
-
-# 	d=do(cn)
-# 	if d in table_data:
-# 		print("added down")
-# 		stack.insert(0,d)
-
-# 	# r=ri(cn)
-# 	# stack.insert(0,r)
-
-# 	# u=up(cn)
-# 	# stack.insert(0,u)
-
-
-# 	visited.append(cn)
-
-
-# 	print("stack before",stack)
-
-# 	cn=stack.pop(0)
-# 	print("current node ", cn)
-
-# 	print("stack after",stack)
-# 	print("visited ",visited)
-
-
-
-# 	if(cn=="e"):
-# 		cn=stack.pop(0)
-# 		cn=stack.pop(0)
 
 
 
